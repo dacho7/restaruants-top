659806d0aca96b9e66373a68b4403c5628d6ce57
dacho7 <davidtobarst@gmail.com>
2022-06-14 12:15:28 -0500

    Navbar implementacion

--- app.vue
+++ app.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <Nav />
+    <div class="container">
+      <h1>Welcome to Restaurantly</h1>
+      <a href="/restaurants">Go to te restaurants</a>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.container {
+  text-align: center;
+  margin-top: 5rem;
+}
+</style>
